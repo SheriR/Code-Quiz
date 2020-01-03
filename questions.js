6523721dafe564a1e32fadad7a8e6a0faa36f867
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-01-03 13:43:39 -0600

    questions added in javascript

--- questions.js
+++ questions.js
@@ -1,25 +1,26 @@
-var questions = [
+const questions = [
   {
-    title: "Commonly used data types DO NOT include:",
+    question: "Commonly used data types DO NOT include?",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
   },
+  
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
+    question: "The condition in an if / else statement is enclosed within ____?",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
   },
 
   {
-    title: "Arrays in JavaScript can be used to store ______.",
+    question: "Arrays in JavaScript can be used to store ______?",
     choices: ["numbers and strings", "other Arrays", "booleans", "all of the above"],
     answer: "all of the above"
-  }
+  },
 
   {
-    title: "String values must be enclosed within ______ when being assinged to variables.",
+    question: "String values must be enclosed within ______ when being assinged to variables?",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes"
-  }
+  },
   
 ];
--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const choicesElement = document.getElementsByClassName('choice-text');
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
-
 
 
 function startQuiz() {
     console.log('started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
-}
+ }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.choices.array.forEach( answer => {
+        const button = document.createElement('button')
+        button.innerText = choices.choices
+        button.classList.add('btn')
+
+        
+    });
 
 }
+
+
 
 function selectAnswer() {
 
 }
 
-const questions = [
-    {
-        question: ''
-
-
-    }
-]
