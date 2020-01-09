5df339da281ff259230e4b78135989926e7f8f7e
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-01-09 04:34:35 -0600

    questions populating

---
+++ assets/javascript2.js
@@ -0,0 +1,108 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const choiceButtonsElement = document.getElementById('choice-buttons');
+const timeLeft = document.getElementById('timeleft');
+const timer = document.getElementById('timer')
+
+
+
+//setting the numerical variables for the functions.. scores and timers.. 
+var score = 0;
+var currentQuestion = -1;
+
+//starts the countdown timer once user clicks the 'start' button
+function start() {
+
+    timeLeft = 75;
+    
+    timer = setInterval(function() {
+        timeLeft--;
+        
+        //proceed to end the game function when timer is below 0 at any time
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
+
+    next();
+}
+
+//stop the timer to end the game 
+function endGame() {
+    clearInterval(timer);
+    }
+
+//store the scores on local storage
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+}
+
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+    }
+
+//clears the score name and value in the local storage if the user selects 'clear score'
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+
+    resetGame();
+}
+
+//reset the game 
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+   
+}
+
+//deduct 15seconds from the timer if user chooses an incorrect answer
+function incorrect() {
+    timeLeft -= 15; 
+    next();
+}
+
+//increases the score by 20points if the user chooses the correct answer
+function correct() {
+    score += 20;
+    next();
+}
+
+//loops through the questions 
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    choicebuttons.addEventListener('click', selectchoice)
+    
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+
+    }
---
+++ assets/questions.js
@@ -0,0 +1,48 @@
+let questions = [
+  {    
+    question: "Commonly used data types DO NOT include?",
+    choiceA: "strings",
+    choiceB: "booleans",
+    choiceC: "alerts",
+    choiceD: "numbers",
+    correct: "C", //"alerts"
+  },
+  
+  {
+    question: "The condition in an if / else statement is enclosed within ____?",
+    choiceA: "quotes",
+    choiceB: "curly brackets",
+    choiceC: "parentheses",
+    choiceD: "square brackets",
+    correct: "C", //"parentheses"
+  },
+
+  {
+    question: "Arrays in JavaScript can be used to store ______?",
+    choiceA: "numbers and strings",
+    choiceB: "other Arrays",
+    choiceC: "booleans",
+    choiceD: "all of the above",
+    correct: "D", //"all of the above"
+  },
+
+  {
+    question: "String values must be enclosed within ______ when being assinged to variables?",
+    choiceA: "commas",
+    choiceB: "curly brackets",
+    choiceC: "quotes",
+    choiceD: "parentheses",
+    correct: "C", //"quotes"
+  },
+
+  {
+    question: "What does JS stand for?",
+    choiceA: "Just Saying",
+    choiceB: "JavaStyle",
+    choiceC: "JavaScript",
+    choiceD: "JScript",
+    correct: "C", //JavaScript
+ 
+  }
+  
+];
