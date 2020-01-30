4a3ede462a31006361014ce33cf8765ed8b1108f
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-01-30 12:17:09 -0600

    got timer to work & can choose answers

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,17 +2,50 @@ const startButton = document.getElementById('start-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
-const timer = document.getElementById('timer')
+//const timer = document.getElementById('timer')
 const highscore = document.getElementById('highscores')
+let timeLeft = 75
+
+function timer(){
+    var sec = 20;
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+// function timerFunction(){
+//     setInterval(function(){
+//         timeleft = timeLeft - 1
+//         timeLeft -= 1
+// if (timeLeft <= 0) {
+//             clearInterval(timer);
+//             endGame()
+
+//     }, 1000)
+// }
+// }
+
+// function startTimer() {
+//     time = 75;
+//     time = setInterval(function(){
+//         timeLeft--;
+//         if (timeLeft <= 0) {
+//             clearInterval(timer);
+//             endGame()
+//         }, 1000)
+//     }
+// }
 
 
-// variables
-var timeLeft; 
 var highScores; //(array);
 let numberCorrect;
 var createDiv = document.createElement('div');
 let shuffledQuestions, currentQuestionIndex;
-let answer; 
+let answer;
 
 
 // Start Quiz:
@@ -21,20 +54,19 @@ let answer;
 // 3 start timer
 
 function startQuiz() {
-    console.log('start game')
+    console.log('start game', questions)
     startButton.classList.add('hide') //hides start button after clicked
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    // shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
+    timer.innerHTML = timeLeft
+    timer()
     questionContainerElement.classList.remove('hide') //removes hide on question-container
-    displayQuestion()
+    showQuestion(questions[currentQuestionIndex])
 };
 
 startButton.addEventListener('click', startQuiz)
 
 
-function displayQuestion(){
-      showQuestion(shuffledQuestions[currentQuestionIndex])
-}
 
 function showQuestion(question) {
     questionElement.innerText = question.question 
@@ -47,7 +79,9 @@ function showQuestion(question) {
     button.classList.add('btn')
     
     answerButtonsElement.appendChild(button)
-    button.addEventListener('click', selectAnswer)
+    button.addEventListener('click', function(event){
+        selectAnswer(event)
+    })
     
 
     console.log(choice)  //this logs choices that can be selected
@@ -61,25 +95,37 @@ function resetState(){
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
         }
     }
+
+
    
     
 function selectAnswer(event){
-        var selectedButton = event.target;
-        console.log(selectedButton.textContent) //this logs what the user selected
-        answer = selectedButton.textcontent
-        questionIndex = 0;
+        // var selectedButton = event.target;
+        // console.log(selectedButton.textContent) //this logs what the user selected
+        // answer = selectedButton.textcontent
+        // questionIndex = 0;
+        let answer = event.srcElement.innerHTML
+        if(answer === questions[currentQuestionIndex].answer){
+            alert("You are correct")
+            currentQuestionIndex++
+            showQuestion(questions[currentQuestionIndex])
+        } else {
+            alert("Incorrect")
+            currentQuestionIndex++
+            showQuestion(questions[currentQuestionIndex])
+        }
      
 
-if (answer === questions[currentQuestionIndex.answer]) {
-        var createDiv = document.createElement("div");
-        createDiv.textContent = "Your answer is Correct!";
-     } 
+// if (answer === questions[currentQuestionIndex.answer]) {
+//         var createDiv = document.createElement("div");
+//         createDiv.textContent = "Your answer is Correct!";
+//      } 
         
-    else {
-         createDiv.textContent = "Wrong!  The correct asnwer is: " + questions[questionIndex].answer;
-        }
+//     else {
+//          createDiv.textContent = "Wrong!  The correct asnwer is: " + questions[currentQuestionIndex].answer;
+//         }
+//     }
     }
-  
 
 
     
